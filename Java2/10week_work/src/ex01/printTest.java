aa24ca27fe9872e3e309d5c777a073d4c8ed8dcf
Juminjae <45286407+Juminjae@users.noreply.github.com>
2022-06-02 20:54:05 +0900

    Java_Archive
    Work archive 61

---
+++ Java2/10week_work/src/ex01/printTest.java
@@ -0,0 +1,22 @@
+package ex01;
+
+public class printTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String verb = "현실이 된다";
+		System.out.println("생각이 " + verb);
+		
+		String s = "1234567";
+		System.out.println("문자열의 길이는 " + s.length());
+		
+		String s1 = "ABCDEFG";
+		s1 = s1.toLowerCase();
+		System.out.println(s1);
+		
+		System.out.println("2 + 3 = "+ (2 + 3));
+		
+		System.out.println("2 + 3 = " + 2 + 3);
+	}
+
+}
---
+++ Java2/10week_work/src/ex01/printTest2.java
@@ -0,0 +1,17 @@
+package ex01;
+
+public class printTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "java programming";
+		String b = a.substring(5);
+		String c = a.substring(5,7);
+		System.out.println(b);
+		System.out.println(c);
+		char d = a.charAt(1);
+		String e = a + d;
+		System.out.println(e);
+	}
+
+}
